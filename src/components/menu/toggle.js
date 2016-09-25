5ee43ffa6a57f5c02692ecf2a1e592efc33eb5ed
Tyom Semonov <inspiro@gmail.com>
2016-09-25 20:07:44 +0100

    Use menu icon

--- src/components/menu/toggle.js
+++ src/components/menu/toggle.js
@@ -1,13 +1,13 @@
 import React, {PropTypes} from 'react';
+import FaEllipsisV from 'react-icons/lib/fa/ellipsis-v';
 import FaAngleLeft from 'react-icons/lib/fa/angle-left';
-import FaAngleRight from 'react-icons/lib/fa/angle-right';
 
 import styles from './styles.css';
 
 const Toggle = ({onClick, isMenuClosed}) => (
   <button className={styles['Menu-toggle']} onClick={onClick} type="button">
     {isMenuClosed
-      ? <FaAngleRight className={styles['Menu-toggleIcon']}/>
+      ? <FaEllipsisV className={styles['Menu-toggleIcon']}/>
       : <FaAngleLeft className={styles['Menu-toggleIcon']}/>
     }
     <span>{isMenuClosed ? 'Open' : 'Close'} menu</span>

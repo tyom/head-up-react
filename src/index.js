093133a9dfcf29d0f01e6f7d1c80ec688b5e3846
Tyom Semonov <inspiro@gmail.com>
2016-10-04 21:01:34 +0100

    Move smart components to containers

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 require('smoothscroll-polyfill').polyfill();
 
-import HeadUp from './components/head-up';
-import Dashboard from './components/dashboard';
+import HeadUp from './containers/head-up';
+import Dashboard from './containers/dashboard';
 import Cell from './components/cell';
 import Menu from './components/menu';
 

2130df56e517417ad82e56240a706abcbbaedde7
Tyom Semonov <inspiro@gmail.com>
2018-03-07 00:27:21 +0000

    Fix Cell tests and add Widget tests

--- src/components/Cell/test.js
+++ src/components/Cell/test.js
@@ -9,74 +9,20 @@ describe('Cell: structure', () => {
   });
 
   test('default component', () => {
-    rendered = render(<Cell />);
+    rendered = shallow(<Cell />);
   });
 
-  test('custom size', () => {
-    rendered = render(<Cell size="1:3" />);
+  test('has size', () => {
+    rendered = shallow(<Cell size="1:3" />);
   });
 
-  test('inner container class name', () => {
-    rendered = render(<Cell innerClassName="useful">content</Cell>).find(
-      '.useful'
+  test('has children', () => {
+    rendered = shallow(<Cell size="2">contents</Cell>);
+  });
+
+  test('has widget', () => {
+    rendered = shallow(
+      <Cell size="2" widget={{ title: 'my data', isActive: true }} />
     );
-  });
-
-  test('active state', () => {
-    rendered = render(<Cell size="2" isActive={true} />);
-  });
-
-  test('activated settings', () => {
-    rendered = render(<Cell title="test" isConfiguring={true} />);
-  });
-
-  test('with children', () => {
-    rendered = render(<Cell size="2">contents</Cell>);
-  });
-});
-
-describe('Cell: behaviour', () => {
-  test('click to activate cell', () => {
-    const clickSpy = jest.fn();
-    const wrapper = shallow(<Cell onClick={clickSpy} />);
-
-    wrapper.simulate('click');
-
-    expect(clickSpy).toHaveBeenCalled();
-  });
-
-  test('shows settings only without children', () => {
-    const wrapperWithChildren = mount(
-      <Cell title="test" isActive={true}>
-        contents
-      </Cell>
-    );
-    const wrapperWithoutChildren = mount(<Cell title="test" isActive={true} />);
-
-    expect(wrapperWithChildren.find('SettingsButton')).toHaveLength(0);
-    expect(wrapperWithoutChildren.find('SettingsButton')).toHaveLength(1);
-  });
-
-  test('click to activate cell settings', () => {
-    const clickSpy = jest.fn();
-    const wrapper = mount(
-      <Cell title="test" isActive={true} onSettingsClick={clickSpy} />
-    );
-    const settingsButton = wrapper.find('SettingsButton');
-
-    settingsButton.simulate('click');
-
-    expect(clickSpy).toHaveBeenCalled();
-  });
-});
-
-describe('Cell: settings', () => {
-  test('clicking settings calls a handler', () => {
-    const clickSpy = jest.fn();
-    const wrapper = shallow(<Cell onClick={clickSpy} />);
-
-    wrapper.simulate('click');
-
-    expect(clickSpy).toHaveBeenCalled();
   });
 });
--- src/components/Widget/test.js
+++ src/components/Widget/test.js
@@ -5,6 +5,40 @@ describe('Widget: structure', () => {
   test('initialise widget', () => {
     const wrapper = shallow(<Widget />);
 
-    console.log(wrapper);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
+
+describe('Widget: behaviour', () => {
+  test('click to activate cell', () => {
+    const clickSpy = jest.fn();
+    const wrapper = shallow(<Widget onClick={clickSpy} />);
+
+    wrapper.simulate('click');
+
+    expect(clickSpy).toHaveBeenCalled();
+  });
+
+  test('click to activate cell settings', () => {
+    const clickSpy = jest.fn();
+    const wrapper = mount(
+      <Widget title="test" isActive={true} onSettingsClick={clickSpy} />
+    );
+    const settingsButton = wrapper.find('SettingsButton');
+
+    settingsButton.simulate('click');
+
+    expect(clickSpy).toHaveBeenCalled();
+  });
+});
+
+describe('Cell: settings', () => {
+  test('clicking settings calls a handler', () => {
+    const clickSpy = jest.fn();
+    const wrapper = shallow(<Widget onClick={clickSpy} />);
+
+    wrapper.simulate('click');
+
+    expect(clickSpy).toHaveBeenCalled();
   });
 });
